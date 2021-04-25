30b021311eb1daf9224edd4bd863ddf61c832fb8
DJDrama <putzzang@naver.com>
2021-04-26 08:21:21 +0900

    Integrating Google map2

--- app/src/main/java/com/dj/brownsmog/ui/home/Home.kt
+++ app/src/main/java/com/dj/brownsmog/ui/home/Home.kt
@@ -1,6 +1,12 @@
 package com.dj.brownsmog.ui.home
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,26 +16,45 @@ import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import com.dj.brownsmog.ui.Screen
+import com.google.android.gms.common.config.GservicesValue.isInitialized
 
 @Composable
 fun Home(viewModel: HomeViewModel, onNavigate: (String) -> Unit) {
 
     val location = viewModel.myLocation.collectAsState()
-    location.value?.let {
+    val context = LocalContext.current
+    val isPermissionGranted = remember { mutableStateOf(false) }
 
-    } ?: NoLocationView(onClick = {
-        val navRoute = Screen.FindLocation.route
-        onNavigate(navRoute)
-    })
+
+    if (isPermissionGranted.value) {
+        location.value?.let {
+
+        } ?: NoLocationView(onClick = {
+            val navRoute = Screen.FindLocation.route
+            onNavigate(navRoute)
+        })
+    } else {
+        // RequestPermissionHandler(onPermissionResult = { isGranted ->
+        //     isPermissionGranted.value = isGranted
+        // }, content = { requestPermissionLauncher ->
+        //     if (!myLocationPermissionGranted(context)) {
+        //         requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        //     }
+        // })
+    }
 }
 
 @Composable
-fun NoLocationView(onClick: ()->Unit) {
+fun NoLocationView(onClick: () -> Unit) {
     Column(modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
@@ -38,5 +63,24 @@ fun NoLocationView(onClick: ()->Unit) {
         Button(onClick = onClick) {
             Text(text = "내 위치 찾기")
         }
+    }
+}
+
+@Composable
+fun RequestPermissionHandler(
+    onPermissionResult: (Boolean) -> Unit,
+    content: @Composable (ActivityResultLauncher<String>) -> Unit,
+) {
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted->
+        onPermissionResult(isGranted)
+    }
+    content(requestPermissionLauncher)
+}
+
+private fun myLocationPermissionGranted(context: Context): Boolean {
+    return arrayOf(Manifest.permission.ACCESS_FINE_LOCATION).all {
+        ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
     }
 }
